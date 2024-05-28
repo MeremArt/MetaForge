799d370488395b57104762d351879e4f91603da6
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-05-28 12:29:24 +0100

    refactor:made more ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../components/Navbar";
 
-import Navbar from '../components/Navbar';
+import WalletContextProvider from "../contexts/WalletContextProvider";
 
-import WalletContextProvider from '../contexts/WalletContextProvider';
-import TransitionContextProvider from '../contexts/TransitionContextProvider';
-import Head from 'next/head';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Helius</title>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔨</text></svg>" />
+        <title>METAFORGE</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔨</text></svg>"
+        />
       </Head>
       <WalletContextProvider>
         <Navbar />
         <ToastContainer />
-        <TransitionContextProvider>
-          <Component {...pageProps} />
-        </TransitionContextProvider>
+
+        <Component {...pageProps} />
       </WalletContextProvider>
     </>
   );
 }
 
-export default MyApp
+export default MyApp;
