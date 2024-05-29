f8ed492e4e9a0217aeea095257bf77a9336313a0
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-05-29 19:39:48 +0100

    add:link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,15 @@ function MyApp({ Component, pageProps }: AppProps) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔨</text></svg>"
         />
+        <meta property="og:url" content="metaforgeprotocol.xyz" />
+        <meta
+          property="og:title"
+          content="Metaforge protocol - ISSUE. MINT. SHARE. REDEEM"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dtfvdjvyr/image/upload/v1717007967/WhatsApp_Image_2024-05-29_at_19.38.39_xftkf9.jpg"
+        />
       </Head>
       <WalletContextProvider>
         <Navbar />
