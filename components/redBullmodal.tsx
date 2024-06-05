17945a2cc4568bb69be9099f35360ed387a6fa16
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-06-06 00:46:39 +0100

    add:blur-redbull

--- components/redBullmodal.tsx
+++ components/redBullmodal.tsx
@@ -24,75 +24,84 @@ const RedModal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
   const currentDate = formatDate(new Date());
   return (
-    <div className={`${styles.modal} ${isOpen ? styles.open : ""}`}>
-      <div className={styles["modal-content"]}>
-        <div className={` rounded-md ${styles["flex-container"]}`}>
-          <div className="w-2/5 rounded-l-md  linear-background">
-            {" "}
-            <span className={styles.close} onClick={onClose}>
-              Back
-            </span>
-            <img
-              src="https://res.cloudinary.com/dtfvdjvyr/image/upload/v1717615406/Frame_146_2_oefyph.png"
-              height={200}
-              width={200}
-            />
-          </div>
-          <div className="rounded-r-md bg-[#0c111d] w-3/5">
-            <div className="flex flex-col items-start px-14 py-10">
-              <h2 className="font-geist text-white font-bold text-xl">
-                A CAN OF REDBULL
-              </h2>
-              <p className="text-sm text-[#c2c2cccb] mt-3">
-                You are about to mint a can of Redbull
-              </p>
-              <hr className="custom-hr mt-5 opacity-20" />
-              <div className="flex justify-between w-full mt-4">
-                <div>
-                  <h2 className="font-geist mr-10 text-white text-sm">
-                    CREATED BY
-                  </h2>
-                  <p className="flex  text-sm text-[#c2c2cccb] mt-3">
-                    {formatAddress(walletAddress) || "Connect Wallet"}
-                  </p>
-                </div>
-                <div>
-                  <h2 className="font-geist text-white text-sm ">
-                    DATE CREATED
-                  </h2>
-                  <p className="flex text-sm text-[#c2c2cccb] mt-3">
-                    {currentDate}
-                  </p>
-                </div>
-              </div>
-              <hr className="custom-hr mt-4 opacity-20" />
-              <div className="flex flex-col items-start mt-4  ">
-                <h2 className="font-geist text-white  text-sm">PRICE</h2>
-                <p className="text-sm text-[#c2c2cccb] ">0.02 SOL</p>
-              </div>
-              <div className="flex flex-col items-start mt-4  ">
-                <h2 className="font-geist text-white  text-sm">
-                  ADDITIONAL INFORMATION
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm "></div>
+      )}
+      <div
+        className={`${styles.modal} ${
+          isOpen ? styles.open : ""
+        } fixed inset-0 flex items-center justify-center`}
+      >
+        <div className={styles["modal-content"]}>
+          <div className={` rounded-md ${styles["flex-container"]}`}>
+            <div className="w-2/5 rounded-l-md  linear-background">
+              {" "}
+              <span className={styles.close} onClick={onClose}>
+                Back
+              </span>
+              <img
+                src="https://res.cloudinary.com/dtfvdjvyr/image/upload/v1717615406/Frame_146_2_oefyph.png"
+                height={200}
+                width={200}
+              />
+            </div>
+            <div className="rounded-r-md bg-[#0c111d] w-3/5">
+              <div className="flex flex-col items-start px-14 py-10">
+                <h2 className="font-geist text-white font-bold text-xl">
+                  A CAN OF REDBULL
                 </h2>
-                <p className="text-sm text-[#c2c2cccb] ">0.02 SOL</p>
-              </div>
-              {/* <div className="flex flex-col items-start mt-4  ">
+                <p className="text-sm text-[#c2c2cccb] mt-3">
+                  You are about to mint a can of Redbull
+                </p>
+                <hr className="custom-hr mt-5 opacity-20" />
+                <div className="flex justify-between w-full mt-4">
+                  <div>
+                    <h2 className="font-geist mr-10 text-white text-sm">
+                      CREATED BY
+                    </h2>
+                    <p className="flex  text-sm text-[#c2c2cccb] mt-3">
+                      {formatAddress(walletAddress) || "Connect Wallet"}
+                    </p>
+                  </div>
+                  <div>
+                    <h2 className="font-geist text-white text-sm ">
+                      DATE CREATED
+                    </h2>
+                    <p className="flex text-sm text-[#c2c2cccb] mt-3">
+                      {currentDate}
+                    </p>
+                  </div>
+                </div>
+                <hr className="custom-hr mt-4 opacity-20" />
+                <div className="flex flex-col items-start mt-4  ">
+                  <h2 className="font-geist text-white  text-sm">PRICE</h2>
+                  <p className="text-sm text-[#c2c2cccb] ">0.02 SOL</p>
+                </div>
+                <div className="flex flex-col items-start mt-4  ">
+                  <h2 className="font-geist text-white  text-sm">
+                    ADDITIONAL INFORMATION
+                  </h2>
+                  <p className="text-sm text-[#c2c2cccb] ">0.02 SOL</p>
+                </div>
+                {/* <div className="flex flex-col items-start mt-4  ">
                 <h2 className="font-geist text-white  text-sm">
                   ADDITIONAL INFORMATION
                 </h2>
                 <p className="text-sm text-[#c2c2cccb] ">0.02 SOL</p>
               </div> */}
-              <div className="flex items-center justify-center mt-6 w-full ">
-                <button className="gradient-button text-center items-center justify-center gap-2 rounded-xl w-full">
-                  MINT
-                </button>
+                <div className="flex items-center justify-center mt-6 w-full ">
+                  <button className="gradient-button text-center items-center justify-center gap-2 rounded-xl w-full">
+                    MINT
+                  </button>
+                </div>
               </div>
             </div>
           </div>
+          {children}
         </div>
-        {children}
       </div>
-    </div>
+    </>
   );
 };
 
